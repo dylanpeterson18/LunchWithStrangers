000bec802fa0c96c0ed7f63762d3b856beea58d5
Dylan Peterson <floridakid411@Dylans-MacBook-Pro.local>
2016-07-06 16:31:20 -0700

    prior to seeding the fake seed date

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,10 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+ActiveRecord::Base.connection.reset_pk_sequence!('users')
+ActiveRecord::Base.connection.reset_pk_sequence!('cities')
+ActiveRecord::Base.connection.reset_pk_sequence!('lunch_events')
+
 #Cities
 NY = City.create!(name: "New York City", image_url: "http://s3.amazonaws.com/tws-production-paperclip/cities/header_bgs/000/000/005/small/nyc2-correct.jpg?1409550576")
 SF = City.create!(name: "San Francisco", image_url: "http://s3.amazonaws.com/tws-production-paperclip/cities/header_bgs/000/000/001/small/sf.jpg?1409550565")
@@ -41,30 +45,35 @@ SherylSandberg = User.create!(username: "SherylSandberg", password: "SherylSandb
 MeganSmith = User.create!(username: "MeganSmith", password: "MeganSmith", city: "San Francisco")
 
 #Lunches
-LizLunch = LunchEvent.create!(date_time: , place:  , host_id: , city_id:  , details: , time: )
-JackLunch = LunchEvent.create!(date_time: , place:  , host_id: , city_id:  , details: , time: )
-JennaLunch = LunchEvent.create!(date_time: , place:  , host_id: , city_id:  , details: , time: )
-JerryLunch = LunchEvent.create!(date_time: , place:  , host_id: , city_id:  , details: , time: )
-CostanzaLunch = LunchEvent.create!(date_time: , place:  , host_id: , city_id:  , details: , time: )
-ElaineLunch = LunchEvent.create!(date_time: , place:  , host_id: , city_id:  , details: , time: )
-TaylorLunch = LunchEvent.create!(date_time: , place:  , host_id: , city_id:  , details: , time: )
-JasonLunch = LunchEvent.create!(date_time: , place:  , host_id: , city_id:  , details: , time: )
-ClooneyLunch = LunchEvent.create!(date_time: , place:  , host_id: , city_id:  , details: , time: )
-KimLunch = LunchEvent.create!(date_time: , place:  , host_id: , city_id:  , details: , time: )
-MichaelLunch = LunchEvent.create!(date_time: , place:  , host_id: , city_id:  , details: , time: )
-JimLunch = LunchEvent.create!(date_time: , place:  , host_id: , city_id:  , details: , time: )
-PamLunch = LunchEvent.create!(date_time: , place:  , host_id: , city_id:  , details: , time: )
-PeterLunch = LunchEvent.create!(date_time: , place:  , host_id: , city_id:  , details: , time: )
-LarryLunch = LunchEvent.create!(date_time: , place:  , host_id: , city_id:  , details: , time: )
-SherylLunch = LunchEvent.create!(date_time: , place:  , host_id: , city_id:  , details: , time: )
-MeganLunch = LunchEvent.create!(date_time: , place:  , host_id: , city_id:  , details: , time: )
+LizLunch = LunchEvent.create!(date_time: "08-22-2016", place: "Hot Dog stand outside 30 Rock" , host_id: 37, city_id: 1 , details: , time: "11:30 AM")
+JackLunch = LunchEvent.create!(date_time: "08-30-2016", place:  "Plunder on E 85th St.", host_id: 38 , city_id: 1, details: "best dessert in NYC - for only $1000! Remember to wear a tuxedo. We're not farmers" , time: "1:00 PM")
+JennaLunch = LunchEvent.create!(date_time: "09-08-2016", place:  "Outback SteakHouse in Greenwich Village", host_id: 39 , city_id: 1, details: "No rules, just right ;)" , time: "3:30 PM")
+JerryLunch = LunchEvent.create!(date_time: "09-14-2016", place:  "Tom's Restaurant W 95th St.", host_id: 40, city_id: 1, details: "None of that 'No soup for you' nonsense", time: "12:00 PM")
+CostanzaLunch = LunchEvent.create!(date_time: "08-09-2016", place:  "Subway near Yankee Stadium", host_id: 41, city_id: 1, details: "I went through all this trouble to host the lunch - the least everyone can do is pitch in for my meal", time: "1:30 PM")
+ElaineLunch = LunchEvent.create!(date_time: "08-25-2016", place:  "Peterman's Office", host_id: 42, city_id: 1, details: "Bring ideas to help with my men's catalog descriptions", time: "2:30 PM")
+TaylorLunch = LunchEvent.create!(date_time: "08-26-2016", place: "Taylor Swifts house", host_id: 43, city_id: 4, details: "I'll be with my boyfriend... TBD by the date of this lunch", time: "2:00 PM")
+JasonLunch = LunchEvent.create!(date_time: "09-03-2016", place: "My Secret Lunch Spot in the warehouse on 2nd Ave", host_id: 44, city_id: 4, details: "Leave any cellphones aka tracking devices at home", time: "1:00 PM")
+ClooneyLunch = LunchEvent.create!(date_time: "09-16-2016", place:  "George Clooneys house", host_id: 45, city_id: 4, details: "Wear sunglasses to stop the sun from reflecting off my perfect smile", time: "1:30 PM")
+KimLunch = LunchEvent.create!(date_time: "09-29-2016", place: "Nobu 903 N La Cienega Blvd", host_id: 46, city_id: 4, details: "No paparazzi please - I already brought my own", time: "12:00 PM")
+MichaelLunch = LunchEvent.create!(date_time: "09-07-2016", place: "Chili's on Wilkes-Barre Rd.", host_id: 47, city_id: 3, details: "I'll be wearing my Michael Klump outift!", time: "11:30 AM")
+JimLunch = LunchEvent.create!(date_time: "08-27-2016", place:  "Poor Richard's Pub on Main Ave.", host_id: 48, city_id: 3, details: "I'll be sitting in the back!", time: "1:00 PM")
+PamLunch = LunchEvent.create!(date_time: "09-01-2016", place:  "The Smoke House Restaurant on Redwood St.", host_id: 49, city_id: 3, details: "Would love to share any artwork! All types and media welcome!", time: "2:00 PM")
+PeterLunch = LunchEvent.create!(date_time: "09-26-2016", place: "Peter Thiel" , host_id: 50, city_id: 2, details: "Peter Thiel", time: "12:30 PM")
+LarryLunch = LunchEvent.create!(date_time: "08-17-2016", place: "Larry Ellison" , host_id: 51, city_id: 2, details: "Larry Ellison", time: "2:00 PM")
+SherylLunch = LunchEvent.create!(date_time: "07-26-2016", place:  "Sheryl", host_id: 52, city_id: 2, details: "Sheryl", time: "3:30 PM")
+MeganLunch = LunchEvent.create!(date_time: "09-09-2016", place:  "Megan", host_id: 53, city_id: 2, details:  "Megan", time: "1:00 PM")
 
 
 
-
-date_time: "07-21-2016",
-place: "jasonbourne",
-host_id: 44,
-city_id: 10,
-details: "bourne",
-time: "1:00 PM">
+# 1 NY
+# 2 SF
+# 3 Boston
+# 4 LA
+#
+#
+# date_time: "07-21-2016",
+# place: "jasonbourne",
+# host_id: 44,
+# city_id: 10,
+# details: "bourne",
+# time: "1:00 PM">
